fc3dadd106b149bae23558945a852a9f67cc9b6b
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-05-01 20:21:34 +0500

    Api Project

--- ReactFullStackWith.Net/Controllers/ProductsController.cs
+++ ReactFullStackWith.Net/Controllers/ProductsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ReactFullStackWith.Net.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
