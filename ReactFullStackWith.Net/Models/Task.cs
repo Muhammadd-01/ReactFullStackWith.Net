3ed9a1fd1ec098b6a5ec71f5e511ab59b7b71d1c
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-05-03 13:12:09 +0500

    FullStackProject

---
+++ ReactFullStackWith.Net/Models/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactFullStackWith.Net.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
