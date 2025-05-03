e573abe17f797d9c17327a00de108f8b1baf7276
Muhammadd-01 <muhammadaffan1445@gmail.com>
2025-05-03 12:41:04 +0500

    Backend

--- ReactFullStackWith.Net/Program.cs
+++ ReactFullStackWith.Net/Program.cs
@@ -1,10 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Allow CORS for react frontend
+builder.Services.AddCors(options =>
+{
+options.AddPolicy("AllowReactFrontend",
+    policy =>
+        {
+    policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+});
+
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+//Enable CORS
+
+app.UseCors("AllowReactFrontend");
 
 // Configure the HTTP request pipeline.
 
